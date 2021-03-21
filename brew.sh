a69f141a0c1a7afc9967f4bde53d93a75954a857
vijaymanthena <33968840+vijaymanthena@users.noreply.github.com>
2021-03-22 09:18:50 +1100

    Create brew.sh

---
+++ brew.sh
@@ -0,0 +1,81 @@
+brew install adns		
+brew install gnu-sed
+brew install libksba
+brew install npth
+brew install ruby
+brew install autoconf
+brew install gnu-tar
+brew install libnet		
+brew install oniguruma	
+brew install ruby-build
+brew install automake	
+brew install gnupg		
+brew install libtasn1	
+brew install openssl@1.1	
+brew install sqlite
+brew install bdw-gc		
+brew install gnutls		
+brew install libtool		
+brew install p11-kit		
+brew install sshpass
+brew install c-ares		
+brew install grep		
+brew install libunistring	
+brew install p7zip		
+brew install tcptraceroute
+brew install coreutils	
+brew install guile		
+brew install libusb		
+brew install pcre		
+brew install tmux
+brew install cscope		
+brew install jemalloc	
+brew install libyaml		
+brew install pinentry	
+brew install tree
+brew install findutils	
+brew install jq		
+brew install lua		
+brew install pkg-config	
+brew install unbound
+brew install gawk		
+brew install libassuan	
+brew install macvim		
+brew install protobuf	
+brew install utf8proc
+brew install gdbm		
+brew install libev		
+brew install moreutils	
+brew install pstree		
+brew install wget
+brew install gettext		
+brew install libevent	
+brew install mosh		
+brew install pyenv		
+brew install xz
+brew install git-flow-avh	
+brew install libffi		
+brew install mpfr		
+brew install zlib
+brew install gmp		
+brew install libgcrypt	
+brew install ncurses		
+brew install rbenv
+brew install gnu-getopt	
+brew install libgpg-error	
+brew install nettle		
+brew install readline
+brew install gnu-indent	
+brew install libidn2		
+brew install nghttp2		
+brew install rlwrap
+
+brew install --cask chrome
+brew install --cask opera
+brew install --cask anaconda
+brew install --cask font-source-code-pro       
+brew install --cask macdown                    
+brew install --cask vagrant-manager
+brew install --cask joplin
+brew install --cask vagrant
+brew install --cask yt-music
